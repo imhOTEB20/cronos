d21b67776af461e0431ef18b38a7bf883e18ed70
imhOTEB20 <crisalbertoaa20@gmail.com>
2023-07-21 14:22:57 -0300

    Se agrago a Main_Cronos la funcion de poder abrir una ventana de edicion de evento al hacer doble clic en uno de los eventos del listbox.

--- Main_Cronos.py
+++ Main_Cronos.py
@@ -7,12 +7,14 @@ from datetime import datetime
 from Logo import Logo
 from Reloj import Reloj
 from DateGrid import DateGrid
-
-import mysql.connector
+from VentanaDetalles import Form
+from DataBase import Datos
+from CenteredTopLevel import CenteredTopLevel
 
 class MainCronos(tk.Frame):
     def __init__(self, parent):
         super().__init__(parent)
+        self._parent = parent
         self._frame = ttk.Frame(self)
         self._frame.grid(row=0, column=0)
         self._logo = Logo(self._frame)
@@ -23,52 +25,51 @@ class MainCronos(tk.Frame):
 
         self._dateGrid = DateGrid(self, self.load_events)
         self._dateGrid.grid(row=1, column=0)
+
+        self._db = Datos(user='root', password='ACL&cag20', host='localhost', database='cronosdb')
         
         self._title_list = tk.StringVar()
         self_label_list = ttk.Label(self, textvariable=self._title_list)
         self_label_list.grid(row=2, column=0)
-        self._list_day_events = tk.Listbox(self, width=40)
+        self._list_day_events = tk.Listbox(self, width=40, selectmode=tk.SINGLE)
         self._list_day_events.grid(row=3, column=0)
+        self._list_day_events.bind('<Double-Button-1>', self._on_double_click_in_event)
         self.load_events()
+    
+    def _on_double_click_in_event(self, event):
+        index  = self._list_day_events.curselection()[0]
+        list = self._db.eventsbyday[self._day][index]
+        eventData = {"id": list[0],
+                "title": list[1],
+                 "duration": list[2],
+                  "datetimeE": list[3],
+                  "datetimeR": list[4],
+                  "detail": list[5],
+                  "relevance": list[6],
+                  "tags": self._db.tags_cross_idEvent(list[0])}
+        editEvento = CenteredTopLevel(self, self._parent)
+        editEvento.title("Editar Evento")
+        Form(editEvento, eventData)
 
-        self._establish_conection()
-    
-    @staticmethod
-    def _establish_conection():
-        conection = mysql.connector.connect(
-            host = "localhost",
-            user = "root",
-            password = "ACL&cag20",
-            database = "cronosdb")
-        return conection
-    
+    def reset_list(self):
+        self._list_day_events.delete(0, tk.END)
+        try:
+            for event in self._db.eventsbyday[self._day]:
+                self._list_day_events.insert(tk.END, event[1])
+        except KeyError as error:
+            self._list_day_events.insert(tk.END, "NO HAY EVENTO ESTE DIA.")
+
     def load_events(self, date:datetime = None):
         if date!=None and (self._clock.today.day!=date.day or self._clock.today.month!=date.month or self._clock.today.year!=date.year):
             self._title_list.set(f"{self._clock._day_name[date.weekday()]} {date.day}")
-            date_str = date.strftime('%Y-%m-%d')
+            self._day = date.day
+            if self._db.date.month!=date.month or self._db.date.year!=date.year:
+                self._db.date = date
         else:
             self._title_list.set('HOY')
-            date_str = self._clock.today.strftime('%Y-%m-%d')
-        
-        self._list_day_events.delete(0, tk.END)
-        conection = self._establish_conection()
-        query = """SELECT titulo,fecha_y_hora_e FROM eventos WHERE DATE(eventos.fecha_y_hora_e) = %s"""
-        cursor = conection.cursor()
-        cursor.execute(query, (date_str,))
-        result = cursor.fetchall()
-        if not result:
-            self._list_day_events.insert(tk.END, "No hay eventos en este dia.")
-        else:
-            for evento in result:
-                hora = evento[1].strftime('%H:%M')
-                self._list_day_events.insert(tk.END, f"{hora} - {evento[0]}")
-        conection.close()
+            self._day = self._clock.today.day
+        self.reset_list()
 
-    def charge_eventos(self):
-        connection = self._establish_conection()
-        cursor = connection.cursor()
-        date = self._dateGrid._date
-        consulta = """SELECT """
 root = tk.Tk()
 app = MainCronos(root)
 app.grid(padx=3,pady=3)
